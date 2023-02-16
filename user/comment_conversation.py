85e675d9419e1131ab1c84d666038a8f81732569
sweetsenpai <32019970+sweetsenpai@users.noreply.github.com>
2023-02-16 17:32:27 +0300

    comment conversation

--- user/comment_conversation.py
+++ user/comment_conversation.py
@@ -8,8 +8,8 @@ RAITING, COMMENT = range(2)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-    parametr = context.args
-    if parametr[0] is None:
+    parametr = context.args[0]
+    if parametr is None:
         return ConversationHandler.END
     inline_raiting = InlineKeyboardMarkup([[InlineKeyboardButton(text= '⭐️⭐️⭐️⭐️⭐️', callback_data=5)],
                                            [InlineKeyboardButton(text= '⭐️⭐️⭐️⭐️', callback_data=4)],
@@ -17,16 +17,19 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
                                            [InlineKeyboardButton(text= '⭐️⭐️', callback_data=2)],
                                            [InlineKeyboardButton(text= '⭐️', callback_data=1)]])
     await update.message.reply_text(text='Оцените мастера по шкале от 1 до 5', reply_markup=inline_raiting)
+    session.add(Review(user_id=update.message.from_user.id,
+                       user_name=update.message.from_user.username,
+                       user_master=int(parametr),
+                       review_text='in progress'))
+    session.commit()
     return RAITING
 
 
 async def leave_comment(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(context.args)
-    # session.add(Review(user_id=update.callback_query.from_user.id,
-    #                    user_name=update.callback_query.from_user,
-    #                    user_))
+
     raitng = update.callback_query.data
-    print(raitng)
+    session.query(Review).where(Review.user_id == update.callback_query.from_user.id and Review.review_text == 'in progress').one().review_rating = raitng
+    session.commit()
     await context.bot.sendMessage(chat_id=update.callback_query.from_user.id,
                                   text='Пришлите ваш отзыв.')
     return COMMENT
@@ -34,7 +37,9 @@ async def leave_comment(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def end(update: Update, context: ContextTypes.DEFAULT_TYPE):
     comment = update.message.text
-    print(comment)
+    session.query(Review).where(
+        Review.user_id == update.message.from_user.id and Review.review_text == 'in progress').one().review_texte = comment
+    session.commit()
     await update.message.reply_text(text='Спасибо за отзыв! \nВаш отзыв будет обупликован после модерации.')
     return ConversationHandler.END
 
