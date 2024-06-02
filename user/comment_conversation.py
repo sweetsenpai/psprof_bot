408446b187302229f0d8ff4b337f01ae5228a431
Sweet_Sempai <chcolatemilk00@gmail.com>
2024-06-02 16:34:48 +0300

    add user_data support, ed user check

--- user/comment_conversation.py
+++ user/comment_conversation.py
@@ -6,7 +6,7 @@ from admin.publish_content import show_review_star
 from datetime import datetime
 import re
 
-RAITING, COMMENT = range(2)
+RAITING, COMMENT, CHECK, REWRITE = range(4)
 
 
 async def main_board(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -32,34 +32,61 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         master = ''.join(re.findall(r'\d+', parametr[0]))
         await show_review_star(update, context, master_id=int(master))
         return ConversationHandler.END
+    context.user_data['master'] = int(parametr[0])
     inline_raiting = InlineKeyboardMarkup([[InlineKeyboardButton(text= '⭐️⭐️⭐️⭐️⭐️', callback_data=5)],
                                            [InlineKeyboardButton(text= '⭐️⭐️⭐️⭐️', callback_data=4)],
                                            [InlineKeyboardButton(text= '⭐️⭐️⭐️', callback_data=3)],
                                            [InlineKeyboardButton(text= '⭐️⭐️', callback_data=2)],
                                            [InlineKeyboardButton(text= '⭐️', callback_data=1)]])
     await update.message.reply_text(text='Оцените мастера по шкале от 1 до 5', reply_markup=inline_raiting)
-    session.add(Review(user_id=update.message.from_user.id,
-                       user_name=update.message.from_user.username,
-                       user_master=int(parametr[0]),
-                       review_text='in progress',
-                       time=datetime.today().strftime('%Y-%m-%d %H:%M')))
-    session.commit()
     return RAITING
 
 
 async def leave_comment(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    raitng = update.callback_query.data
-    session.query(Review).where(Review.user_id == update.callback_query.from_user.id).where(Review.review_text == 'in progress').first().review_rating = raitng
-    session.commit()
-    await context.bot.sendMessage(chat_id=update.callback_query.from_user.id,
-                                  text='Напишите мне Ваш отзыв о мастере')
-    await update.callback_query.answer()
-    return COMMENT
+    if update.callback_query.data is not None:
+        raitng = update.callback_query.data
+        context.user_data['raiting'] = raitng
+        await context.bot.sendMessage(chat_id=update.callback_query.from_user.id,
+                                      text='Напишите мне Ваш отзыв о мастере')
+        await update.callback_query.answer()
+    else:
+        await update.message.reply_text(text='Напишите мне Ваш отзыв о мастере')
+    return CHECK
+
+
+async def user_chek_comment(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data['text'] = update.message.text
+    await update.message.reply_text('Проерьте ваш  комментарий перед отправкой')
+    await update.message.reply_text(text=f"<i>{context.user_data['text']}</i>", parse_mode='HTML')
+    await update.message.reply_text(text='Отправить сообщение в исходном виде?', reply_markup=ReplyKeyboardMarkup([[KeyboardButton(text='Да'), KeyboardButton(text='Исправить')]]))
+    return REWRITE
+
+
+async def user_rewritecomment(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.text =='Да':
+        review = Review(user_id=update.message.from_user.id,
+                           user_name=update.message.from_user.username,
+                           user_master=context.user_data['master'],
+                           review_text=context.user_data['text'],
+                           review_rating=context.user_data['raiting'],
+                           time=datetime.today().strftime('%Y-%m-%d %H:%M'))
+        session.add(review)
+        session.commit()
+        await update.message.reply_text(text='Спасибо! Ваш отзыв будет опубликован после проверки модератором')
+        await send_comment_to_modderation(context, review)
+        return ConversationHandler.END
+    await update.message.reply_text(text='Напишите мне новый отзыв о мастере')
+    return CHECK
 
 
 async def end(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    review = session.query(Review).where(Review.user_id == update.message.from_user.id, Review.review_text == 'in progress').first()
-    review.review_text = update.message.text
+    review = Review(user_id=update.message.from_user.id,
+                    user_name=update.message.from_user.username,
+                    user_master=context.user_data['master'],
+                    review_text=context.user_data['text'],
+                    review_rating=context.user_data['raiting'],
+                    time=datetime.today().strftime('%Y-%m-%d %H:%M'))
+    session.add(review)
     session.commit()
     await update.message.reply_text(text='Спасибо! Ваш отзыв будет опубликован после проверки модератором')
     await send_comment_to_modderation(context, review)
@@ -70,8 +97,10 @@ new_comment_conversation = ConversationHandler(
     entry_points=[CommandHandler('start', start)],
     states={
         RAITING: [CallbackQueryHandler(pattern='[1-5]', callback=leave_comment)],
+        CHECK: [MessageHandler(filters.TEXT, user_chek_comment)],
+        REWRITE: [MessageHandler(callback=user_rewritecomment, filters=filters.ALL)],
         COMMENT: [MessageHandler(filters.TEXT, end)]},
-    fallbacks=[CommandHandler('stop', stop_conversation)])
+    fallbacks=[CommandHandler('stop', stop_conversation)], allow_reentry=True)
 
 
 async def send_comment_to_modderation(context: ContextTypes.DEFAULT_TYPE, review: Review):
